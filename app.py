1fa50e2a49c2756bf0c764f31f4dc5421a8d6261
Laurent Theroux-Bombardier <therouxbl@hotmail.fr>
2014-11-30 16:42:21 -0500

    modification de membres

--- app.py
+++ app.py
@@ -16,10 +16,36 @@ def index():
 def register_get():
     return render_template('register.html')
 
-
 @app.route('/register', methods=['POST'])
 def register_post():
+    registration = db.cursor()
+    registration.execute(
+        "insert into user(username, password, nom, prenom, courriel, codePostal, adresse, permission, dette) "
+        "values('%(username)s','%(password)s','%(nom)s','%(prenom)s','%(courriel)s','%(codePostal)s','%(adresse)s',1,0) "
+        % request.values
+    )
+    db.commit()
     return "Registration reussie <a href='/'>OK</a>"
+
+@app.route('/membres/<username>', methods=['GET'])
+def membre_get(username):
+    user = db.cursor()
+    user.execute("select * from user where username='%s'" % username)
+    return render_template('membre.html', user=user.fetchone())
+
+
+@app.route('/membres/<username>/modification', methods=['POST'])
+def membre_modification(username):
+    user = db.cursor()
+    update = \
+        "update user " \
+        "set username='%(username)s', password='%(password)s', nom='%(nom)s', " \
+        "prenom='%(prenom)s', courriel='%(courriel)s'," \
+        " codePostal='%(codePostal)s', adresse='%(adresse)s'," \
+        " permission='%(permission)s', dette='%(dette)s' " % request.values
+    update += "where username='%s'" % username
+    user.execute(update)
+    return flask.redirect('/membres')
 
 @app.route('/membres', methods=['GET'])
 def membres_get():
@@ -44,6 +70,16 @@ def livres_delete():
     db.commit()
 
     return flask.redirect('/livres')
+
+@app.route('/membres/delete', methods=['POST'])
+def membres_delete():
+    username = request.values['username']
+
+    delete = db.cursor()
+    delete.execute("DELETE FROM user WHERE username='%s'" % username)
+    db.commit()
+
+    return flask.redirect('/membres')
 
 
 if __name__ == '__main__':
